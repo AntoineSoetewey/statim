bfae9ffd592b8a076984f3adde3dbce157cec2ce
unknown <joshstat126@gmail.com>
2026-06-18 13:55:17 +0800

    Refactorization: Adapting changes for "claim" semantics

--- R/htest-proptest-defs.R
+++ R/htest-proptest-defs.R
@@ -11,7 +11,19 @@ ptest_def = test_define(
                     conf.level = .ci
                 )
                 ptest_build(res, .proc, .ci, .p = .true_p %||% .p)
-            }
+            },
+            claim_parser = map_claim(
+                .p = function(claim, processed) claim_scalar(claim, solve_coef = TRUE)$scalar,
+                .true_p = function(claim, processed) claim_scalar(claim, solve_coef = FALSE)$scalar,
+                .alt = function(claim, processed) {
+                    switch(
+                        claim@op,
+                        "==" = , "!=" = "two.sided",
+                        ">=" = , ">" = "less",
+                        "<=" = , "<" = "greater"
+                    )
+                }
+            )
         ),
         prop = variant(
             fn = function(.proc, .p = 0.5, .alt = "two.sided", .ci = 0.95, .true_p = NULL, correct = TRUE) {
@@ -24,34 +36,20 @@ ptest_def = test_define(
                     correct = correct
                 )
                 ptest_build(res, .proc, .ci, .p = .true_p %||% .p)
-            }
+            },
+            claim_parser = map_claim(
+                .p = function(claim, processed) claim_scalar(claim)$scalar,
+                .true_p = function(claim, processed) claim_scalar(claim, solve_coef = FALSE)$scalar,
+                .alt = function(claim, processed) {
+                    switch(
+                        claim@op,
+                        "==" = , "!=" = "two.sided",
+                        ">=" = , ">" = "less",
+                        "<=" = , "<" = "greater"
+                    )
+                }
+            )
         )
     ),
-    compatible_params = list(PI),
-    claim_translator = claim_translate(
-        default = map_claim(
-            .p = function(claim, processed) claim_scalar(claim, solve_coef = TRUE)$scalar,
-            .true_p = function(claim, processed) claim_scalar(claim, solve_coef = FALSE)$scalar,
-            .alt = function(claim, processed) {
-                switch(
-                    claim@op,
-                    "==" = , "!=" = "two.sided",
-                    ">=" = , ">" = "less",
-                    "<=" = , "<" = "greater"
-                )
-            }
-        ),
-        prop = map_claim(
-            .p = function(claim, processed) claim_scalar(claim)$scalar,
-            .true_p = function(claim, processed) claim_scalar(claim, solve_coef = FALSE)$scalar,
-            .alt = function(claim, processed) {
-                switch(
-                    claim@op,
-                    "==" = , "!=" = "two.sided",
-                    ">=" = , ">" = "less",
-                    "<=" = , "<" = "greater"
-                )
-            }
-        )
-    )
+    compatible_params = list(PI)
 )
