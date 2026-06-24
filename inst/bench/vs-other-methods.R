e5fb2fd6bfc0d8f30ce092ced2e05ad5f19676f0
unknown <joshstat126@gmail.com>
2026-06-24 13:40:29 +0800

    A few line exchanges

--- inst/bench/vs-other-methods.R
+++ inst/bench/vs-other-methods.R
@@ -7,27 +7,28 @@ box::use(
         define_model, `%by%`, TTEST, prepare_test,
         state_null, via, conclude
     ],
-    infer[specify, hypothesize, calculate, t_test],
+    infer[specify, hypothesize, calculate, t_test1 = t_test],
     stats[t.test],
-    dplyr[slice_sample]
+    dplyr[slice_sample],
+    rstatix[t_test2 = t_test]
 )
 
 sleep_large = slice_sample(sleep, n = 1e6L, replace = TRUE, by = group)
 
 bench::mark(
-    `base R-1` = t.test(extra ~ group, sleep_large),
     statim1 = TTEST(extra %by% group, sleep_large),
     statim2 = {
         sleep_large |>
             define_model(extra %by% group) |>
             prepare_test(TTEST) |>
             state_null(
-                MU(extra, group == "2") - MU(extra, group == "1") == 0
+                MU(extra, group == "2") - 2 * MU(extra, group == "1") == 0
             ) |>
             via("contrast") |>
             conclude()
     },
     statim3 = TTEST(extra ~ group, sleep_large),
+    `base R-1` = t.test(extra ~ group, sleep_large),
     `base R-2` = t.test(
         sleep_large$extra[sleep_large$group == "1"],
         sleep_large$extra[sleep_large$group == "2"]
@@ -38,7 +39,8 @@ bench::mark(
             hypothesize(null = "independence") |>
             calculate(stat = "t", order = c("2", "1"))
     },
-    infer2 = t_test(sleep_large, extra ~ group),
+    infer2 = t_test1(sleep_large, extra ~ group),
+    rstatix = t_test2(sleep_large, extra ~ group),
     check = FALSE,
     memory = FALSE
 )
