98b0e19f6c8479a6bbffb0afc27e223a6f1539bc
unknown <joshstat126@gmail.com>
2026-07-08 11:17:48 +0800

    Change `x` to `var_id`

--- R/define-var-ids-process.R
+++ R/define-var-ids-process.R
@@ -2,7 +2,7 @@
 #'
 #' A function for development use to extract the information in Variable Mappers.
 #'
-#' @param x The Variable Mappers to be extracted.
+#' @param var_id The Variable Mappers to be extracted.
 #' @param data Optional. Only passed when a certain data structure (normally it's data frame)
 #'    is required.
 #' @param ... Passed through S7 method compatibility.
@@ -15,20 +15,20 @@
 #' @export
 model_processor = S7::new_generic(
     "model_processor",
-    "x",
-    fun = function(x, data = NULL, ...) S7::S7_dispatch()
+    "var_id",
+    fun = function(var_id, data = NULL, ...) S7::S7_dispatch()
 )
 
-S7::method(model_processor, var_id) = function(x, data = NULL, ...) {
+S7::method(model_processor, var_id) = function(var_id, data = NULL, ...) {
     list()
 }
 
-S7::method(model_processor, S7::class_formula) = function(x, data = NULL, ...) {
-    vars = all.vars(x)
+S7::method(model_processor, S7::class_formula) = function(var_id, data = NULL, ...) {
+    vars = all.vars(var_id)
     data = if (rlang::is_null(data)) {
         vctrs::new_data_frame(rlang::set_names(
             lapply(vars, \(v) {
-                rlang::eval_tidy(rlang::sym(v), env = rlang::f_env(x))
+                rlang::eval_tidy(rlang::sym(v), env = rlang::f_env(var_id))
             }),
             vars
         ))
@@ -36,27 +36,27 @@ S7::method(model_processor, S7::class_formula) = function(x, data = NULL, ...) {
         data
     }
 
-    list(data = data, vars = vars, formula = x)
+    list(data = data, vars = vars, formula = var_id)
 }
 
-S7::method(model_processor, x_by) = function(x, data = NULL, ...) {
-    proc = two_vars_extract(x@x, x@group, data = data, role2 = "group")
+S7::method(model_processor, x_by) = function(var_id, data = NULL, ...) {
+    proc = two_vars_extract(var_id@x, var_id@group, data = data, role2 = "group")
     list(x_data = proc$x1_data, group_data = proc$x2_data)
 }
 
-S7::method(model_processor, rel) = function(x, data = NULL, ...) {
-    proc = two_vars_extract(x@x, x@resp, data = data, role2 = "resp")
+S7::method(model_processor, rel) = function(var_id, data = NULL, ...) {
+    proc = two_vars_extract(var_id@x, var_id@resp, data = data, role2 = "resp")
     list(x_data = proc$x1_data, resp_data = proc$x2_data)
 }
 
-S7::method(model_processor, pairwise) = function(x, data = NULL, ...) {
-    pairwise_data_extract(x, data)
+S7::method(model_processor, pairwise) = function(var_id, data = NULL, ...) {
+    pairwise_data_extract(var_id, data)
 }
 
-S7::method(model_processor, prop) = function(x, data = NULL, ...) {
-    list(x = x@x, n = x@n)
+S7::method(model_processor, prop) = function(var_id, data = NULL, ...) {
+    list(x = var_id@x, n = var_id@n)
 }
 
-S7::method(model_processor, on) = function(x, data = NULL, ...) {
-    multiple_vars_extract(x, data)
+S7::method(model_processor, on) = function(var_id, data = NULL, ...) {
+    multiple_vars_extract(var_id, data)
 }
